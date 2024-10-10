0151aa08822358b0a6d82f07f55d5783b366f8ab
Kristof Grenson <kristof@nizer.be>
2024-10-10 13:53:24 +0200

    html table

---
+++ 20241010/index.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" data-lt-installed="true">
+
+<head>
+    <link rel="icon" href="https://via.placeholder.com/70x70">
+    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
+
+    <meta charset="utf-8">
+    <meta name="description" content="WikiWisKwis">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>WikiWisKwis - Overzicht</title>
+</head>
+
+<body>
+    <main>
+        <section>
+            <header>
+                <h1>WikiWisKwis</h1>
+            </header>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Datum</th>
+                        <th>Titel</th>
+                        <th>Tip</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><b>01/01/2024</b></td>
+                        <td>WikiWisKwis999</td>
+                        <td>Dit is de tip</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+
+        </section>
+    </main>
+
+</body>
+
+</html>
--- 20241010/wikis.php
+++ 20241010/wikis.php
@@ -4,25 +4,25 @@ $wikis = array(
     0 => array(
         'episode' => '06/10/2024',
         'title' => 'WikiWisKwis 001',
-        'data' => 'data/wwk001.php',
+        'data' => 'data/wwk0001.php',
         'tip' => 'trend',
     ),
     1 => array(
         'episode' => '07/10/2024',
         'title' => 'WikiWisKwis 002',
-        'data' => 'data/wwk002.php',
+        'data' => 'data/wwk0002.php',
         'tip' => 'Floki',
     ),
     2 => array(
         'episode' => '08/10/2024',
         'title' => 'WikiWisKwis 003',
-        'data' => 'data/wwk003.php',
+        'data' => 'data/wwk0003.php',
         'tip' => 'Good boy',
     ),
     3 => array(
         'episode' => '09/10/2024',
         'title' => 'WikiWisKwis 004',
-        'data' => 'data/wwk004.php',
+        'data' => 'data/wwk0004.php',
         'tip' => 'GOT',
     ),
 );
