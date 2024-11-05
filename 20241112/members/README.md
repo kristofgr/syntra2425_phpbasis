8934e291bbfb08704a89e5995a85ddd63ca45ce0
Kristof Grenson <kristof@nizer.be>
2024-11-05 15:18:20 +0100

    opdracht members

---
+++ 20241112/members/README.md
@@ -0,0 +1,16 @@
+# Opdracht Members...
+
+Maak een member registration form met volgende velden:
+
+- Voornaam (verplicht, max 100)
+- Naam (verplicht, max 100)
+- Username (verplicht, max 20, uniek)
+- Gender (verplicht, m|f|x)
+- photo (optioneel, URL naar afbeelding online)
+
+Maak daarbij gebruik van de members table in bijgevoegde members.sql
+
+Voorzie twee pagina:
+
+1. registration form met formulier + validatie + duidelijke foutboodschappen + feedback indien ok
+2. een admin pagina waar een overzicht getoond wordt van alle members en ik members kan deleten indien
---
+++ 20241112/members/members.sql
@@ -0,0 +1,45 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table members
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `members`;
+
+CREATE TABLE `members` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `lastname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `gender` varchar(1) NOT NULL DEFAULT 'x',
+  `photo` varchar(255) DEFAULT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+
+INSERT INTO `members` (`id`, `firstname`, `lastname`, `username`, `gender`, `photo`, `created`, `updated`)
+VALUES
+	(1,'Johnny','Appleseed','japple','m','https://s3-us-east-2.amazonaws.com/aws2-gray-wp01-s4/appleholler-graydientlabs-net/wp-content/uploads/2016/09/18184407/johnny-appleseed-portrait-190x180.jpg','2024-11-05 15:11:24','2024-11-05 15:11:24');
+
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
